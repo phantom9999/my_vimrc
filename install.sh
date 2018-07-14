943c221f80efc930c853f825742cf383f686fed3
phantom9999 <1057369361@qq.com>
2018-07-15 00:02:18 +0800

    rm

--- install.sh
+++ install.sh
@@ -1,4 +1,3 @@
 rm -rf  bundle
 mkdir bundle
-git clone https://github.com/gmarik/vundle.git bundle/vundle
-git clone https://github.com/vim-scripts/pathogen.vim.git bundle/pathogen.vim
+git clone https://github.com/VundleVim/Vundle.vim.git bundle/Vundle.vim
